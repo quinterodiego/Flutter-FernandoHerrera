282f9c7705e9e660389d00c45a98b866a5b5b5e8
quinterodiego <d86webs@gmail.com>
2023-05-21 13:50:51 -0300

    asd

---
+++ 01-dart-intro/02_variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  final String pokemon = 'Ditto';
+  final int hp = 100;
+  final bool isAlive = true;
+  final abilities = ['impostor'];
+  final sprites = ['ditto/front.png', 'ditto/back.png'];
+  
+  print("""
+      $pokemon
+      $hp
+      $isAlive
+      $abilities
+      $sprites
+  """);
+}
